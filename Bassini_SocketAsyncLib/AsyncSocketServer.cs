e177d93c96412d6d0f264ef55056e6594aed9d97
ffex <kurogan21@gmail.com>
2021-01-08 11:07:45 +0100

    Initial commit

---
+++ Bassini_SocketAsyncLib/AsyncSocketServer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bassini_SocketAsyncLib
+{
+    public class AsyncSocketServer
+    {
+        IPAddress mIP;
+        int mPort;
+        TcpListener mServer;
+
+        // Mette in ascolto il server
+        public async void InizioAscolto(IPAddress ipaddr = null, int port = 23000)
+        {
+            //faccio dei controlli su IPAddress e sulle porte
+            if (ipaddr == null)
+            {
+                //mIP = IPAddress.Any;
+                ipaddr = IPAddress.Any;
+            }
+            if(port <0 || port > 65535)
+            {
+                //mPort = 23000;
+                port = 23000;
+            }
+
+            mIP = ipaddr;//aggiunte
+            mPort = port;//aggiunte
+
+            Debug.WriteLine("Avvio il server. IP: {0} - Porta: {1}",mIP.ToString(),mPort.ToString());
+            //creare l'oggetto server
+            mServer = new TcpListener(mIP, mPort);
+
+            //avviare il server
+            mServer.Start();
+
+            // mettermi in ascolto
+            TcpClient client = await mServer.AcceptTcpClientAsync();
+            Debug.WriteLine("Client connesso: " + client.Client.RemoteEndPoint);
+
+            RiceviMessaggi(client);
+        }
+        public async void RiceviMessaggi(TcpClient client)
+        {
+           
+            NetworkStream stream = null;
+            StreamReader reader = null;
+            try
+            {
+                stream = client.GetStream();
+                reader = new StreamReader(stream);
+                char[] buff = new char[512];
+
+                //ricezione effettiva
+                while (true)
+                {
+                    Debug.WriteLine("Pronto ad ascoltare...");
+                    int nBytes = await reader.ReadAsync(buff, 0, buff.Length);
+                    Debug.WriteLine("Returned bytes:" + nBytes);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Debug.WriteLine(ex.Message);
+            }
+
+        }
+
+    }
+}
